dca655b72a0fbcbfb3fc5bca29844ac9604efea2
Katiane <katianemiranda00@gmail.com>
2022-06-26 18:57:26 -0300

    feat: :sparkles: add funcao soma

---
+++ math.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//import "fmt"
+
+func main() {
+	fmt.Println(Soma(10, 10))
+
+}
+
+//Soma - realiza a soma de dois números
+func Soma(a int, b int) int {
+	return a + b
+}
