abbe949397d0d5b36106d71ab4787bf30d715893
Katiane <katianemiranda00@gmail.com>
2022-07-25 20:50:21 -0300

    build: Correcao funcao Soma

--- math.go
+++ math.go
@@ -5,15 +5,12 @@ import "fmt"
 //import "fmt"
 
 func main() {
-	fmt.Println(soma(20, 10))
-<<<<<<< HEAD
-	fmt.Println(soma(40, 10))
-=======
->>>>>>> 816555e33681dc01628d252ca5c534206e1aeb06
+	fmt.Println(Soma(20, 10))
+	fmt.Println(Soma(40, 10))
 
 }
 
 //Soma - realiza a soma de dois números
-func soma(a int, b int) int {
+func Soma(a int, b int) int {
 	return a + b
 }
